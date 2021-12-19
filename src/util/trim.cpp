0a2c48522b5ee8cdfd69248543cca3dd4a05c04c
chp <chp@localhost.localdomain>
2021-12-19 19:49:58 +0900

    [#10] first commit

---
+++ src/util/trim.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+using namespace std;
+
+#include "trim.h"
+
+string& trim(string &str)
+{
+	return ltrim(rtrim(str));
+}
+
+string trim_copy(string str)
+{
+	return ltrim(rtrim(str));
+}
+
+string& ltrim(string &str)
+{
+	str.erase(str.begin(), find_if(str.begin(), str.end(), [](unsigned char c) {return !isspace(c);}));
+
+	return str;
+}
+
+string ltrim_copy(string str)
+{
+	return ltrim(str);
+}
+
+string& rtrim(string &str)
+{
+	str.erase(find_if(str.rbegin(), str.rend(), [](unsigned char c) {return !isspace(c);}).base(), str.end());
+
+	return str;
+}
+
+string rtrim_copy(string str)
+{
+	return rtrim(str);
+}
